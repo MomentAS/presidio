bb98fc00e5458a4754b8281d4c91cffaa11d39c9
Ovidiu Pop <ovidiu-dan.pop@eddagroup.com>
2025-04-29 09:56:29 +0300

    Set da in supported_languages in default recognizers and analyzer_engine

--- presidio-analyzer/presidio_analyzer/analyzer_engine.py
+++ presidio-analyzer/presidio_analyzer/analyzer_engine.py
@@ -56,7 +56,7 @@ class AnalyzerEngine:
         context_aware_enhancer: Optional[ContextAwareEnhancer] = None,
     ):
         if not supported_languages:
-            supported_languages = ["en"]
+            supported_languages = ["en", "da"]
 
         if not nlp_engine:
             logger.info("nlp_engine not provided, creating default.")
